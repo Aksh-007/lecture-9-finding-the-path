2c843ae47a4f29ca88e3d54c6b5cea8538b2f827
Aksh-007 <akshaybarapatre54@gmail.com>
2023-03-30 14:05:12 +0530

    Header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [variable, setvariable] = useState("LogOut");
+  return (
+    <nav
+      className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
+      style={{ marginBottom: "50px" }}
+    >
+      <div className="container flex flex-wrap items-center justify-between mx-auto items-center ">
+        <Link to="/" className="flex items-center">
+          {/* <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-6 mr-3 sm:h-9"
+            alt="Flowbite Logo"
+          /> */}
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            SavoryBite
+          </span>
+        </Link>
+        <div className="flex md:order-2">
+          <Link to="/cart">
+            <button
+              type="button"
+              className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white  rounded-lg hover:bg-blue-800 "
+            >
+              <p>Cart</p>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-cart"
+                  viewBox="0 0 16 16"
+                >
+                  {" "}
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />{" "}
+                </svg>
+              </svg>
+              <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+                0
+              </div>
+            </button>
+          </Link>
+        </div>
+        <div className="flex md:order-2">
+          <Link to="/signin">
+            <button
+              type="button"
+              className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white  rounded-lg hover:bg-blue-800 "
+              onClick={() =>
+                setvariable(variable === "LogIn" ? "LogOut" : "LogIn")
+              }
+            >
+              <span className="w-10">{variable}</span>
+            </button>
+          </Link>
+        </div>
+        <div
+          className="items-center justify-between  hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex items-center flex-col p-4 gap-[80px] mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                to="/"
+                className="block py-2 pl-3 pr-4  text-white bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-white"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/service"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
