dd076ec1943db8268164b6a12b77f9ab2e49b809
Aksh-007 <akshaybarapatre54@gmail.com>
2023-03-30 14:05:25 +0530

    About Page Created

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+const About = () =>(
+    <div className="mt-[100px] h-[50vh]" style={{color:'white'}}>
+        <h1>About Pages</h1>
+        <p>Finding the path</p>
+    </div>
+)
+
+export default About
